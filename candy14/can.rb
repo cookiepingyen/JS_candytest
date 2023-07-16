f463a2289ca62b2d6e0cebb7729c7c15cd750d3a
Ping Yen-Wu <panthercookie@gmail.com>
2023-07-17 07:22:25 +0800

    modify candy 14, 15 ruby answer

--- candy14/can.rb
+++ candy14/can.rb
@@ -3,11 +3,18 @@
 # 題目：把鄰近的重複值去除，但仍照原本的順序排序
 # 範例："AAABBBDDDAABBBCC" -> ['A', 'B', 'D', 'A', 'B', 'C']
 
-# 先聲明一個空陣列且放入第一個element, 在從第二個開始判段是不是跟前一個相同
+# 如果是字串就分割成陣列,然後篩選出第一個或是跟前一比不同的值
+# def uniqueOrder(sequence) 
+#   sequence = sequence.split("") if sequence.class == String
+#   sequence.select.with_index {|n , i| i == 0 || n != sequence[i-1] }
+# end
+
 def uniqueOrder(sequence) 
-  sequence = sequence.split("") if (sequence.class == String)
-  return sequence.select.with_index {|n , index| index == 0 || n != sequence[index-1] }
+  sequence = sequence.join("") if sequence.is_a?(Array)
+  sequence.squeeze.split('')
 end
+
+
 
 p uniqueOrder("AABCC") # [ 'A', 'B', 'C']
 p uniqueOrder("AAABBBCCBCC") # [ 'A', 'B', 'C', 'B', 'C']
