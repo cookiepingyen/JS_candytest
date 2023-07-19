83027a89254234fb38d01b2e143dc195ba1728cb
Ping Yen-Wu <panthercookie@gmail.com>
2023-07-19 11:10:06 +0800

    modify candy 16~18

--- candy17/can.rb
+++ candy17/can.rb
@@ -3,9 +3,15 @@
 # 題目：計算數字的 2 進位裡有幾個 1
 # 範例：5 -> 101 -> 2 個 1
 
+# def countBits(num) 
+#   #將數字轉成二進位的字串,在分割成陣列後, 回傳陣列=="1"的長度
+#   return num.to_s(2).split("").filter{|n| n.to_i.odd? }.length
+# end
+
+
 def countBits(num) 
-  #將數字轉成二進位的字串,在分割成陣列後, 回傳陣列=="1"的長度
-  return num.to_s(2).split("").filter{|n| n.to_i.odd? }.length
+  #將數字轉成二進位的字串,在分割成陣列後, 回傳"1"的數量
+  return num.to_s(2).count("1")
 end
 
 p countBits(1234) # 5
--- candy18/can.rb
+++ candy18/can.rb
@@ -3,10 +3,19 @@
 # 題目：實作一個可以印出隨機整數的函數
 
 
+#引數如果只有一個就回傳
+# def randomNumber(*args) 
+#   return (0..args.first-1).to_a.sample if args.length ==1
+#   (args.first..args[1]-1).to_a.sample
+# end
+
+
+
 def randomNumber(*args) 
-  return (0..args[0]-1).to_a.sample if args.length ==1
-  (args[0]..args[1]-1).to_a.sample
+  return rand(0..args.first-1) if args.length ==1
+  rand(args.first..args[1]-1)
 end
+
 
 p randomNumber(50) # 隨機印出 0 ~ 49 之間的任何一個數字
 p randomNumber(5, 30) # 隨機印出 5 ~ 29 之間的任何一個數字
